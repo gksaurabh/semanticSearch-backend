b97e39d539eacc2f13c8b08b3ba88611c46cf9ce
Saurabh Kishore <gks210199@gmail.com>
2023-07-30 23:15:19 -0400

    trying to use a new build file

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+pip install --upgrade pip
+pip install -r requirements.txt
+pip install flask
+pip install openai -q
+pip install openai
+pip install pandas
+pip install plotly
+pip install sklearn
+pip install scipy
+pip install pyrebase
+pip install pyrebase4
